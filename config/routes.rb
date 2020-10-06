bad3165c8b6ab4c2665fc6093030771493491c07
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2020-10-06 18:53:27 -0500

    add user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'about', to: 'welcome#about'
+  root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :articles, only: [:show, :index, :new, :create,:edit, :update,:destroy]
   resources :articles
-
-  root 'welcome#index'
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 end
