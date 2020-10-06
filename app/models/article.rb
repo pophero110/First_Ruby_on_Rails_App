d96500f477b679fa295f8ffe2c8e3a8ea27b741a
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2020-10-06 17:59:53 -0500

    add one-to-many association between users and articles from backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :text, presence: true
+  validates :title, presence: true
+  validates :text, presence: true
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_203434) do
+ActiveRecord::Schema.define(version: 2020_10_06_225020) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
