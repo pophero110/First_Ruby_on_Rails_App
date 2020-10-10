6a16a0e59c586738174b3ccb4cc0125f299443da
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2020-10-09 20:50:50 -0500

    make controller methods as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(articles_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
