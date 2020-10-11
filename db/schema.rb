b3b4093661a98ed216e5f866a17fa335efcdbe4f
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2020-10-10 19:26:10 -0500

    create Category model and testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_030322) do
+ActiveRecord::Schema.define(version: 2020_10_11_002050) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,13 @@ ActiveRecord::Schema.define(version: 2020_10_10_030322) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-  endrails
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
