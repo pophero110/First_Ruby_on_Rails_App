4d0f8a32446793c2bb031bd40354b0ff60f273a0
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2020-10-21 22:41:57 -0500

    add skip_before_action :verify_authenticity_token to all controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :set_articles, only: [:update, :edit, :show, :destroy]
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:update, :edit, :destroy]
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :require_admin, except: [:index, :show]
 
   def new
