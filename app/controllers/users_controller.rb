ce5b42345da7cf6530c855aa37d7df4dc7f0dec2
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2020-10-28 20:22:50 -0500

    swtich from sqlite3 to postgresql

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signup"
-      redirect_to articles_path
+      flash[:notice] = "Welcome, #{@user.username}, you have successfully signup"
+      redirect_to root_path
     else
       render "new"
     end
