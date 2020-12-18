0e921932f325c64f9f7caa2e22f9b1024dc45cda
Jiajin Ou <jiajin.webdeveloper@gmail.com>
2020-12-18 00:54:35 -0600

    change ui

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    puts params
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "Logged in successfully"
-      redirect_to user
+      redirect_to root_path
     else
       flash.now[:alert] = "There was something wrong with your login detail"
       render "new"
